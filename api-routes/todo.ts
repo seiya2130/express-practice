b83290f2f2d16371b0c1f9ac83b174419dbd6283
seiya2130 <satohseiya0312@gmail.com>
2023-01-30 21:32:44 +0900

    追加・更新追加

--- api-routes/todo.ts
+++ api-routes/todo.ts
@@ -14,11 +14,32 @@ router.get('/:id', async (req, res) => {
     res.json(todo);
 });
 
+router.post('/', async(req, res) => {
+    const todo  = new Todo(req.body);
+    const newTodo = await todo.save();
+    res.json(newTodo);
+});
+
+router.patch('/:id', async (req, res) => {
+    const id = req.params.id;
+    const todo = await Todo.findById(id);
+
+    const { name, status, deadline} = req.body;
+
+    if(todo !== null){
+        todo.name = name;
+        todo.status = status;
+        todo.deadline = deadline
+        await todo!.save();
+    }
+
+    res.json(todo);
+});
+
 router.delete('/:id', async (req, res) => {
     const id = req.params.id;
     await Todo.deleteOne({ id });
     res.json({ "msg" : "Delete Succeeded"});
 });
-
 
 export default router;
